16952f3c17187f4ab1ba027a41d4c34698821264
Niklas <niklas.seggern@stud.hawk.de>
2025-01-29 16:17:25 +0100

    Änderungen vom 29.01. Follow Line weiter gemacht.

--- src/follow_line_node.cpp
+++ src/follow_line_node.cpp
@@ -6,9 +6,13 @@
 #include "cv_bridge/cv_bridge.h"
 #include <opencv2/imgproc/imgproc.hpp>  // Für Bildbearbeitungsfunktionen
 #include <opencv2/highgui/highgui.hpp>  // Für Imshow und Window-Funktionen
+#include <cmath>
+
 
 
 ros::Publisher cmd_vel_pub;
+
+static int last_middle_x = 480; 
 
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
@@ -104,42 +108,60 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     cv::Mat img = cv_ptr->image;
     cv::Mat hsv, mask;
 
+    /*int roi_height = img.rows / 3;  // Nutze nur das untere Drittel
+    cv::Rect roi(0, img.rows - roi_height, img.cols, roi_height);
+    cv::Mat img_roi = img(roi);
+    */
     // In HSV umwandeln
     cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
 
     // Gelb-Farbsegmentierung
-    cv::Scalar lower_yellow(20, 100, 100);
-    cv::Scalar upper_yellow(40, 255, 255);
+    cv::Scalar lower_yellow(20, 0, 0);
+    cv::Scalar upper_yellow(70, 255, 255);
     cv::inRange(hsv, lower_yellow, upper_yellow, mask);
 
     // Konturen finden
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
+    // Konturen ins Bild zeichnen (Rot)
+    //cv::drawContours(img, contours, -1, cv::Scalar(0, 0, 255), 2);
+
     std::vector<int> line_centers;
-
-    for (const auto& contour : contours) {
-        cv::Rect bounding_box = cv::boundingRect(contour);
-        int x_center = bounding_box.x + bounding_box.width / 2;
-        line_centers.push_back(x_center);
-    }
-
-    // Sortiere x-Koordinaten der Linien
-    std::sort(line_centers.begin(), line_centers.end());
 
     int img_center = img.cols / 2;
     int middle_x = img_center;
+    
+    if(contours.size() > 0){
+        for (const auto& contour : contours) {
+            cv::Rect bounding_box = cv::boundingRect(contour);
+            int x_center = bounding_box.x + bounding_box.width / 2;
+            line_centers.push_back(x_center);
+        }
+        
 
-    if (line_centers.size() >= 2) {
-        middle_x = (line_centers[0] + line_centers[1]) / 2;
+        // Sortiere x-Koordinaten der Linien
+        std::sort(line_centers.begin(), line_centers.end());
+
+        if (line_centers.size() >= 2) {
+            
+            middle_x = (line_centers[0] + line_centers[1]) / 2;
+            last_middle_x = middle_x;
+        }
+        else {
+            // Falls keine Linien gefunden wurden, verwende den letzten Wert
+            middle_x = last_middle_x;
+
+        }
     }
+
 
     int error = middle_x - img_center;
 
     // Steuerbefehl berechnen
     geometry_msgs::Twist cmd_vel;
-    cmd_vel.linear.x = 0.2;  // Konstante Vorwärtsgeschwindigkeit
-    cmd_vel.angular.z = -0.002 * error;  // Proportionalregelung für Lenkung
+    cmd_vel.linear.x = 0.08;  // Konstante Vorwärtsgeschwindigkeit
+    cmd_vel.angular.z = -0.001 * error;  // Proportionalregelung für Lenkung
 
     // Begrenzung der Drehgeschwindigkeit
     cmd_vel.angular.z = std::max(std::min(cmd_vel.angular.z, 0.5), -0.5);
@@ -147,11 +169,16 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     cmd_vel_pub.publish(cmd_vel);
 
     // Debug-Anzeige
-    cv::line(img, cv::Point(line_centers[0], 0), cv::Point(line_centers[0], img.rows), cv::Scalar(255, 0, 0), 2);
-    cv::line(img, cv::Point(line_centers[1], 0), cv::Point(line_centers[1], img.rows), cv::Scalar(255, 0, 0), 2);
-    cv::line(img, cv::Point(middle_x, 0), cv::Point(middle_x, img.rows), cv::Scalar(0, 255, 0), 2);
+    if (line_centers.size() > 0) {
+        cv::line(img, cv::Point(line_centers[0], 0), cv::Point(line_centers[0], img.rows), cv::Scalar(255, 0, 0), 2);
+        cv::line(img, cv::Point(line_centers[1], 0), cv::Point(line_centers[1], img.rows), cv::Scalar(255, 0, 0), 2);
+        cv::line(img, cv::Point(middle_x, 0), cv::Point(middle_x, img.rows), cv::Scalar(0, 255, 0), 2);
+    }
 
     cv::imshow("Lane Detection", img);
+    cv::imshow("Maske", mask);
+    cv::imshow("HSV", hsv);
+    //cv::imshow("img_roi", img_roi);
     cv::waitKey(1);
 }
 
